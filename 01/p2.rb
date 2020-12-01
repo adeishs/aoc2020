44a044430d75f43498c04ec9529b56b281a0410b
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-02 09:14:51 +1100

    Make it more idiomatic Ruby

--- 01/p2.rb
+++ 01/p2.rb
@@ -1,18 +1,12 @@
 #!/usr/bin/env ruby
 
-nums = []
-STDIN.each_line do |line|
-  nums.append(line.to_i)
+nums = STDIN.each_line.map do |line|
+  line.to_i
 end
 
-n = nums.count
-for i in 0..n - 3
-  for j in i..n - 2
-    for k in j..n - 1
-      if nums[i] + nums[j] + nums[k] == 2020
-        puts (nums[i] * nums[j] * nums[k])
-        exit
-      end
-    end
+nums.combination(3) do |c|
+  if c.sum == 2020
+    puts c.reduce(&:*)
+    exit
   end
 end
