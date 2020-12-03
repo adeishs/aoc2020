4dc7dc40673bd5045c35342097131a993b436741
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-04 10:58:26 +1100

    rubocop

--- 02/p1.rb
+++ 02/p1.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-puts STDIN.each_line.select {
-  |line| ->(line) {
+puts $stdin.each_line.select { |password_line|
+  lambda { |line|
     (min, max, val_ch, password) = line.split(/[- :]+/)
-    password.split('').select { |c| c == val_ch }.count.
-      between?(min.to_i, max.to_i)
-  }[line]
+    password.split('').select { |c| c == val_ch }.count
+            .between?(min.to_i, max.to_i)
+  }[password_line]
 }.count
--- 02/p2.rb
+++ 02/p2.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-puts STDIN.each_line.select {
-  |line| ->(line) {
+puts $stdin.each_line.select { |password_line|
+  lambda { |line|
     (min, max, val_ch, password) = line.split(/[- :]+/)
     (password[min.to_i - 1] == val_ch) ^ (password[max.to_i - 1] == val_ch)
-  }[line]
+  }[password_line]
 }.count
