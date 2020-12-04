c19229618f061e7e96069014ef22483914200777
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-04 20:47:03 +1100

    Shorten code

--- 04/p2.rb
+++ 04/p2.rb
@@ -1,37 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-buf = ''
-passports = []
-$stdin.each_line.map(&:chomp).map do |line|
-  if line != ''
-    buf += (buf == '' ? '' : ' ') + line
-  else
-    passports.append(buf)
-    buf = ''
-  end
-end
-
-valid_count = 9
-passports.map do |passport|
+def valid_passport(passport)
   els = passport.split(/[ :]/)
   h = {}
   (0...els.count).step(2).map { |i| h[els[i]] = els[i + 1] }
-  h.delete('cid')
 
-  next unless h.keys.count == 7 &&
-              h['byr'].to_i.between?(1920, 2002) &&
-              h['iyr'].to_i.between?(2010, 2020) &&
-              h['eyr'].to_i.between?(2020, 2030) &&
-              ((!h['hgt'].rindex('cm').nil? &&
-                h['hgt'].sub('cm', '').to_i.between?(150, 193)) ||
-               (!h['hgt'].rindex('in').nil? &&
-                h['hgt'].sub('in', '').to_i.between?(59, 76))) &&
-              h['hcl'].match(/^#[\da-f]{6}$/) &&
-              %w[amb blu brn gry grn hzl oth].any? { |c| c == h['ecl'] } &&
-              h['pid'].match(/^\d{9}$/)
-
-  valid_count += 1
+  h.keys.count == 7 &&
+  h['byr'].to_i.between?(1920, 2002) &&
+  h['iyr'].to_i.between?(2010, 2020) &&
+  h['eyr'].to_i.between?(2020, 2030) &&
+  ((!h['hgt'].rindex('cm').nil? &&
+    h['hgt'].sub('cm', '').to_i.between?(150, 193)) ||
+   (!h['hgt'].rindex('in').nil? &&
+    h['hgt'].sub('in', '').to_i.between?(59, 76))) &&
+  h['hcl'].match(/^#[\da-f]{6}$/) &&
+  %w[amb blu brn gry grn hzl oth].any? { |c| c == h['ecl'] } &&
+  h['pid'].match(/^\d{9}$/)
 end
 
-puts valid_count
+puts $stdin.read
+           .split("\n\n")
+           .map { |line| valid_passport(line.gsub("\n", ' ')
+                                            .gsub(/cid:\d+ ?/, '')) }
+           .select { |valid| valid }
+           .count
