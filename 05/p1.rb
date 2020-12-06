fc4d864054ce5e954f6cbfe22c8a239e71710555
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-06 22:23:21 +1100

    rubocop

--- 05/p1.rb
+++ 05/p1.rb
@@ -8,8 +8,7 @@ end
 
 puts $stdin.each_line
            .map(&:chomp)
-           .map {
-             |pass| bin_to_dec(pass[0, 7], 'B') * 8 +
-                    bin_to_dec(pass[7, 3], 'R')
+           .map { |pass|
+             bin_to_dec(pass[0, 7], 'B') * 8 + bin_to_dec(pass[7, 3], 'R')
            }
            .max
