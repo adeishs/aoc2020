2b20b594e01e2711039ae2595d156f9efdb36167
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-08 08:23:51 +1100

    Tidy up a bit

--- 07/p1.rb
+++ 07/p1.rb
@@ -3,33 +3,41 @@
 target = 'shiny gold'
 specs = STDIN.each_line
   .select { |line| !(line.start_with?(target) || line.include?("no other ")) }
-  .map { |line| line.gsub(/ bags?/, '').gsub(/\.$/, '').chomp }
+  .map { |line| line.chomp.gsub(/ bags?/, '').gsub(/\.$/, '') }
 
 contained_by = {}
 specs.map { |spec|
   lambda { |spec|
     (containing, contained) = spec.split(' contain ')
     contained_by[containing] = contained.split(', ').map {
-      |c|
-      lambda { |amt_clr|
-        (amt, clr) = amt_clr.split(' ', 2)[1]
-      }.call(c)
+      |c| c.split(' ', 2)[1]
     }
   }.call(spec)
 }
 
 contained_by.keys.each do |k|
-  (1..10).each do
+  while true
+    finished = true
     tmp = contained_by[k].clone
     contained_by[k].each do |clr|
-      if contained_by[clr] != nil
+      if !contained_by[clr].nil?
         contained_by[clr].each do |i|
-          tmp.append(i) if !tmp.include?(i)
+          if !tmp.include?(i)
+            tmp.append(i)
+            finished = false
+          end
         end
       end
     end
+
+    if finished
+      break
+    end
+
     contained_by[k] = tmp
   end
 end
 
-puts contained_by.keys.select { |k| contained_by[k].include?(target) }.size
+puts contained_by.keys
+                 .select { |k| contained_by[k].include?(target) }
+                 .size
