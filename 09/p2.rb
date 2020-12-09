42fed1721a93b73733a4073e87d54d2bb9ebff98
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-09 17:53:54 +1100

    Solve part 2

---
+++ 09/p2.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+raw_nums = $stdin.each_line
+                 .map { |line| line.chomp.to_i }
+nums = raw_nums.clone
+
+CHUNK_SIZE = 25
+invalid_num = 0
+loop do
+  new_num = nums[CHUNK_SIZE]
+
+  if nums[0, CHUNK_SIZE].combination(2)
+                        .select { |p| p.sum == new_num }
+                        .empty?
+    invalid_num = new_num
+    break
+  end
+
+  nums.shift
+end
+
+puts invalid_num
+
+(2...raw_nums.size).each do |set_size|
+  (0...raw_nums.size - set_size).each do |start|
+    chunk_nums = raw_nums[start, set_size]
+    if chunk_nums.sum == invalid_num
+      puts chunk_nums.min + chunk_nums.max
+      exit
+    end
+  end
+end
