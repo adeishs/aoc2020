66048a50ab66bd3a8fe4e3a6f1577115f9c69dd4
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-10 16:26:58 +1100

    Solve day 10 part 1

---
+++ 10/p1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+joltages = $stdin.each_line
+  .map { |line| line.chomp.to_i }
+  .sort
+
+diff_freq = Hash.new(0)
+diff_freq[joltages[0]] = 1
+(1...joltages.size).each { |i| diff_freq[joltages[i] - joltages[i - 1]] += 1 }
+
+puts diff_freq[1] * (diff_freq[3] + 1)
