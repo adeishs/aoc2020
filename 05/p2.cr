960747f6cd186d6a0a8eefb378ed4422873161ac
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-11 09:33:29 +1100

    Shorten code

--- 05/p2.cr
+++ 05/p2.cr
@@ -1,14 +1,7 @@
 #!/usr/bin/env crystal
 
-def bin_to_dec(str, set_char)
-  (0...str.size).map { |i| str[str.size - 1 - i] == set_char ? 1 << i : 0 }
-    .sum
-end
-
 passes = STDIN.each_line
-  .map { |pass|
-    bin_to_dec(pass[0, 7], 'B') * 8 + bin_to_dec(pass[7, 3], 'R')
-  }
+  .map { |pass| pass.tr("BRFL", "1100").to_i(2) }
   .to_a
   .sort
 
--- 05/p2.rb
+++ 05/p2.rb
@@ -1,15 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def bin_to_dec(str, set_char)
-  (0...str.size).map { |i| str[str.size - 1 - i] == set_char ? 1 << i : 0 }
-                .sum
-end
-
 passes = $stdin.each_line
-               .map do |pass|
-                 bin_to_dec(pass[0, 7], 'B') * 8 + bin_to_dec(pass[7, 3], 'R')
-               end
+               .map { |pass| pass.tr('BRFL', '1100').to_i(2) }
                .sort
 
 (0...passes.size - 1).each do |i|
