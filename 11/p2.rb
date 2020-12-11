b11add0ea2b1d749e43104e5d4a781ac3fdb63ca
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-12 08:53:33 +1100

    Solve part 2

---
+++ 11/p2.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+EMPTY = 'L'
+OCCUPIED = '#'
+FLOOR = '.'
+
+seat_rows = $stdin.each_line.map(&:chomp).to_a
+
+prev = seat_rows.clone
+length = [seat_rows.size, seat_rows[0].size].max
+curr = []
+
+loop do
+  (0...seat_rows.size).each do |i|
+    curr[i] = FLOOR * prev[i].size
+    (0...prev[i].size).each do |j|
+      case prev[i][j]
+      when EMPTY
+        occupied_seen = false
+        (-1..1).each do |y|
+          (-1..1).each do |x|
+            next if x.zero? && y.zero?
+
+            (1..length).each do |dist|
+              row = i + dist * y
+              col = j + dist * x
+
+              break if !row.between?(0, seat_rows.size - 1) ||
+                !col.between?(0, seat_rows[0].size - 1)
+              next if prev[row][col] == FLOOR
+
+              if prev[row][col] == OCCUPIED
+                occupied_seen = true
+              end
+              break
+            end
+            break if occupied_seen
+          end
+
+          break if occupied_seen
+        end
+        curr[i][j] = occupied_seen ? EMPTY : OCCUPIED
+      when OCCUPIED
+        num_of_occupied_seats = 0
+        (-1..1).each do |y|
+          (-1..1).each do |x|
+            next if x.zero? && y.zero?
+
+            (1..length).each do |dist|
+              row = i + dist * y
+              col = j + dist * x
+
+              break if !row.between?(0, seat_rows.size - 1) ||
+                !col.between?(0, seat_rows[0].size - 1)
+              next if prev[row][col] == FLOOR
+
+              if prev[row][col] == OCCUPIED
+                num_of_occupied_seats += 1
+              end
+              break
+            end
+          end
+        end
+
+        curr[i][j] = num_of_occupied_seats >= 5 ? EMPTY : prev[i][j]
+      else
+        curr[i][j] = prev[i][j]
+      end
+    end
+  end
+
+  break if prev == curr
+  prev = curr.clone
+  curr = []
+end
+
+puts curr.map { |row| row.split('').select { |c| c == OCCUPIED }.size }
+         .sum
