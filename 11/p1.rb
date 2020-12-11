48901491cdb5115e3f6eec2f530a87a5bcb3213b
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-11 18:04:29 +1100

    rubocop

--- 11/p1.rb
+++ 11/p1.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 EMPTY = 'L'
 OCCUPIED = '#'
@@ -11,19 +12,20 @@ curr = []
 
 loop do
   (0...prev.size).each do |i|
-    min_dy = i == 0 ? 0 : -1
+    min_dy = i.zero? ? 0 : -1
     max_dy = i < prev.size - 1 ? 1 : 0
 
     curr[i] = EMPTY * prev[i].size
     (0...prev[i].size).each do |j|
-      min_dx = j == 0 ? 0 : -1
+      min_dx = j.zero? ? 0 : -1
       max_dx = j < prev[i].size - 1 ? 1 : 0
       case prev[i][j]
       when EMPTY
         occupied = false
         (min_dy..max_dy).each do |y|
           (min_dx..max_dx).each do |x|
-            next if x == 0 && y == 0
+            next if x.zero? && y.zero?
+
             if prev[i + y][j + x] == OCCUPIED
               occupied = true
               break
@@ -36,10 +38,9 @@ loop do
         num_of_occupied_seats = 0
         (min_dy..max_dy).each do |y|
           (min_dx..max_dx).each do |x|
-            next if x == 0 && y == 0
-            if prev[i + y][j + x] == OCCUPIED
-              num_of_occupied_seats += 1
-            end
+            next if x.zero? && y.zero?
+
+            num_of_occupied_seats += 1 if prev[i + y][j + x] == OCCUPIED
           end
         end
 
@@ -51,8 +52,10 @@ loop do
   end
 
   break if prev == curr
+
   prev = curr.clone
   curr = []
 end
 
-puts curr.map { |row| row.split("").select { |c| c == OCCUPIED }.size }.sum
+puts curr.map { |row| row.split('').select { |c| c == OCCUPIED }.size }
+         .sum
