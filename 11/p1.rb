f176c20bdd0ba8d5e7ab0424cc08ad66f27aafd1
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-12 13:59:51 +1100

    Refactor like part 2

--- 11/p1.rb
+++ 11/p1.rb
@@ -4,58 +4,48 @@
 EMPTY = 'L'
 OCCUPIED = '#'
 FLOOR = '.'
+DS = [-1, 0, 1].freeze
+DIRS = DS.product(DS).reject { |y, x| x.zero? && y.zero? }
 
-seat_rows = $stdin.each_line.map(&:chomp).to_a
-
-prev = seat_rows.clone
-curr = []
-
-loop do
-  (0...prev.size).each do |i|
-    min_dy = i.zero? ? 0 : -1
-    max_dy = i < prev.size - 1 ? 1 : 0
-
-    curr[i] = EMPTY * prev[i].size
-    (0...prev[i].size).each do |j|
-      min_dx = j.zero? ? 0 : -1
-      max_dx = j < prev[i].size - 1 ? 1 : 0
-      case prev[i][j]
-      when EMPTY
-        occupied = false
-        (min_dy..max_dy).each do |y|
-          (min_dx..max_dx).each do |x|
-            next if x.zero? && y.zero?
-
-            if prev[i + y][j + x] == OCCUPIED
-              occupied = true
-              break
-            end
-          end
-          break if occupied
-        end
-        curr[i][j] = OCCUPIED unless occupied
-      when OCCUPIED
-        num_of_occupied_seats = 0
-        (min_dy..max_dy).each do |y|
-          (min_dx..max_dx).each do |x|
-            next if x.zero? && y.zero?
-
-            num_of_occupied_seats += 1 if prev[i + y][j + x] == OCCUPIED
-          end
-        end
-
-        curr[i][j] = num_of_occupied_seats >= 4 ? EMPTY : prev[i][j]
-      else
-        curr[i][j] = prev[i][j]
-      end
-    end
-  end
-
-  break if prev == curr
-
-  prev = curr.clone
-  curr = []
+def outside_boundary?(row, col, seats)
+  !row.between?(0, seats.size - 1) || !col.between?(0, seats[0].size - 1)
 end
 
-puts curr.map { |row| row.split('').select { |c| c == OCCUPIED }.size }
-         .sum
+def get_new_state(seats, i, j)
+  return FLOOR if seats[i][j] == FLOOR
+
+  num_of_occupied_seats = 0
+  DIRS.each do |y, x|
+    row = i + y
+    col = j + x
+
+    next if outside_boundary?(row, col, seats) || seats[row][col] == FLOOR
+
+    num_of_occupied_seats += 1 if seats[row][col] == OCCUPIED
+    return EMPTY if (num_of_occupied_seats.positive? && seats[i][j] == EMPTY) ||
+                    (num_of_occupied_seats >= 4 && seats[i][j] == OCCUPIED)
+  end
+
+  OCCUPIED
+end
+
+def arrange_seats(seats)
+  prev = seats
+  curr = []
+
+  loop do
+    curr = (0...prev.size).map do |i|
+      (0...prev[i].size).map { |j| get_new_state(prev, i, j) }
+    end
+
+    return curr if prev == curr
+
+    prev = curr.clone
+  end
+end
+
+puts arrange_seats($stdin.each_line
+                         .map { |line| line.chomp.split('') })
+  .flatten
+  .select { |c| c == OCCUPIED }
+  .size
