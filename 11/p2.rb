da5de098da2649de1a68d18de2851de85885c892
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-12 12:49:49 +1100

    DRY

--- 11/p2.rb
+++ 11/p2.rb
@@ -4,12 +4,31 @@
 EMPTY = 'L'
 OCCUPIED = '#'
 FLOOR = '.'
-DS = [-1, 0, 1]
+DS = [-1, 0, 1].freeze
 DIRS = DS.product(DS).reject { |y, x| x.zero? && y.zero? }
 
 def outside_boundary?(row, col, seat_rows)
   !row.between?(0, seat_rows.size - 1) ||
     !col.between?(0, seat_rows[0].size - 1)
+end
+
+def check_seats(seats, length, i, j)
+  num_of_occupied_seats = 0
+  DIRS.each do |y, x|
+    (1..length).each do |dist|
+      row = i + dist * y
+      col = j + dist * x
+
+      break if outside_boundary?(row, col, seats)
+      next if seats[row][col] == FLOOR
+
+      num_of_occupied_seats += 1 if seats[row][col] == OCCUPIED
+      break
+    end
+    break if num_of_occupied_seats.positive? && seats[i][j] == EMPTY
+  end
+
+  num_of_occupied_seats
 end
 
 seat_rows = $stdin.each_line.map(&:chomp).to_a
@@ -22,42 +41,14 @@ loop do
   (0...seat_rows.size).each do |i|
     curr[i] = FLOOR * seat_rows[i].size
     (0...prev[i].size).each do |j|
-      case prev[i][j]
-      when EMPTY
-        occupied_seen = false
-        DIRS.each do |y, x|
-          (1..length).each do |dist|
-            row = i + dist * y
-            col = j + dist * x
-
-            break if outside_boundary?(row, col, seat_rows)
-            next if prev[row][col] == FLOOR
-
-            occupied_seen = prev[row][col] == OCCUPIED
-            break
-          end
-          break if occupied_seen
+      curr[i][j] =
+        if prev[i][j] == EMPTY
+          check_seats(prev, length, i, j).positive? ? EMPTY : OCCUPIED
+        elsif prev[i][j] == OCCUPIED && check_seats(prev, length, i, j) >= 5
+          EMPTY
+        else
+          prev[i][j]
         end
-        curr[i][j] = occupied_seen ? EMPTY : OCCUPIED
-      when OCCUPIED
-        num_of_occupied_seats = 0
-        DIRS.each do |y, x|
-          (1..length).each do |dist|
-            row = i + dist * y
-            col = j + dist * x
-
-            break if outside_boundary?(row, col, seat_rows)
-            next if prev[row][col] == FLOOR
-
-            num_of_occupied_seats += 1 if prev[row][col] == OCCUPIED
-            break
-          end
-        end
-
-        curr[i][j] = num_of_occupied_seats >= 5 ? EMPTY : prev[i][j]
-      else
-        curr[i][j] = prev[i][j]
-      end
     end
   end
 
