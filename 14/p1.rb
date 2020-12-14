cd74b75b76c91f6c466f7cfcc57ee7588f067a92
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-14 21:05:28 +1100

    Tidy up

--- 14/p1.rb
+++ 14/p1.rb
@@ -1,25 +1,21 @@
 #!/usr/bin/env ruby
-
-ALL_SET = ('1' * 36).to_i(2)
-
-ins = $stdin.each_line.map(&:chomp)
+# frozen_string_literal: true
 
 mask = '1' * 36
 mem = {}
 
-ins.each do |i|
-  (op, val_s) = i.split(" = ")
+$stdin.each_line.map(&:chomp).each do |i|
+  (op, val_s) = i.split(' = ')
 
-  if op == "mask"
+  if op == 'mask'
     mask = val_s
     next
   end
 
-  loc = op.gsub("mem[", "").gsub("]", "").to_i
+  loc = op.gsub(/(^mem\[|\]$)/, '').to_i
+  val = val_s.to_i.to_s(2).rjust(36, '0').split('')
 
-  val = val_s.to_i.to_s(2).rjust(36, "0").split("")
-
-  (0...mask.split("").size).each do |b|
+  (0...mask.split('').size).each do |b|
     next if mask[b] == 'X'
     val[b] = mask[b]
   end
