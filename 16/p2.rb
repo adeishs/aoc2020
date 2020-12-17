b6adaaa5edfccf5d1ab370a633fd5eacf267818f
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-17 12:14:36 +1100

    rubocop

--- 16/p2.rb
+++ 16/p2.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'set'
-
 def get_valid_tickets(field_str)
   valid_ticket = Hash.new(false)
   field_str.scan(/\d+-\d+/).flatten.each do |range|
@@ -14,9 +12,9 @@ def get_valid_tickets(field_str)
 end
 
 def parse_field_range_set_str(field_range_set_str)
-    field_range_set_str.split(' or ').map do |field_range_str|
-      Range.new(*field_range_str.split('-').map(&:to_i)).to_a
-    end.reduce { |m, o| m | o }
+  field_range_set_str.split(' or ').map do |field_range_str|
+    Range.new(*field_range_str.split('-').map(&:to_i)).to_a
+  end.reduce { |m, o| m | o }
 end
 
 def get_field_tickets(field_str)
@@ -42,11 +40,8 @@ def get_field_order(field_tickets_in, nearby_recs_in)
   fields = []
   (0...nearby_recs.size).each do |i|
     possible_fields[i] = {}
-    good_field = nil
-    field_tickets.keys.each do |field|
-      if (nearby_recs[i] - field_tickets[field]).empty?
-        possible_fields[i][field] = true
-      end
+    field_tickets.each_key do |field|
+      possible_fields[i][field] = true if (nearby_recs[i] - field_tickets[field]).empty?
     end
   end
 
@@ -54,13 +49,15 @@ def get_field_order(field_tickets_in, nearby_recs_in)
     min_size = field_tickets.size
     min_idx = nil
     (0...possible_fields.size).each do |i|
-      if possible_fields[i].keys.size > 0 && possible_fields[i].keys.size < min_size
-        min_size = possible_fields[i].keys.size
-        min_idx = i
-      end
+      next unless possible_fields[i].keys.size.positive? &&
+                  possible_fields[i].keys.size < min_size
+
+      min_size = possible_fields[i].keys.size
+      min_idx = i
     end
-    
+
     break if min_idx.nil?
+
     field = possible_fields[min_idx].keys.first
     fields[min_idx] = field
     (0...possible_fields.size).each { |i| possible_fields[i].delete(field) }
@@ -75,11 +72,15 @@ field_str = sections.shift
 
 valid_ticket = get_valid_tickets(field_str)
 field_tickets = get_field_tickets(field_str)
-#nearby_recs = get_nearby_recs(nearby_str)
+
 nearby_recs = get_nearby_recs(nearby_str).reject do |tix|
   tix.any? { |t| !valid_ticket[t] }
 end
 fields = get_field_order(field_tickets, nearby_recs)
+
 your_tickets = sections.shift.gsub("your ticket:\n", '').split(',').map(&:to_i)
-departure_idxs = (0...fields.size).select { |i| fields[i].start_with?('departure') }
+departure_idxs = (0...fields.size).select do |i|
+  fields[i].start_with?('departure')
+end
+
 puts departure_idxs.map { |i| your_tickets[i] }.reduce(1, :*)
