4e3b1e9345780556c4ed1e8f42c7a69fb397f588
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-18 13:42:12 +1100

    Tidy up

--- 17/p1.rb
+++ 17/p1.rb
@@ -3,45 +3,45 @@
 
 INACTIVE = '.'
 ACTIVE = '#'
-DIRS = [-1, 0, 1]
+DIRS = [-1, 0, 1].freeze
 
 curr = {}
 
 $stdin.each_line
-  .map(&:chomp)
-  .map.with_index do |line, y|
-    line.split('').map.with_index { |c, x| curr[[x, y, 0]] = c }
+      .map(&:chomp)
+      .map.with_index do |line, y|
+  line.split('').map.with_index { |c, x| curr[[x, y, 0]] = c }
 end
 
-6.times {
+6.times do
   nxt = {}
   points = {
-    x: curr.keys.map { |x, y, z| x }.uniq,
-    y: curr.keys.map { |x, y, z| y }.uniq,
-    z: curr.keys.map { |x, y, z| z }.uniq
+    x: curr.keys.map { |x, _, _| x }.uniq,
+    y: curr.keys.map { |_, y, _| y }.uniq,
+    z: curr.keys.map { |_, _, z| z }.uniq
   }
 
-  min = points.map { |k, v| [k, v.min - 1] }.to_h
-  max = points.map { |k, v| [k, v.max + 1] }.to_h
+  min = points.transform_values { |v| v.min - 1 }
+  max = points.transform_values { |v| v.max + 1 }
 
   (min[:z]..max[:z]).to_a
-    .product((min[:y]..max[:y]).to_a, (min[:x]..max[:x]).to_a).each do |z, y, x|
-
+                    .product((min[:y]..max[:y]).to_a, (min[:x]..max[:x]).to_a)
+                    .each do |z, y, x|
     c = curr.fetch([x, y, z], INACTIVE)
-    neigh_coords = [x - 1, x, x + 1].product([y - 1, y, y + 1], [z - 1, z, z + 1])
-      .reject { |nx, ny, nz| nx == x && ny == y && nz == z }
+    active_cube_count = [x - 1, x, x + 1].product(
+      [y - 1, y, y + 1], [z - 1, z, z + 1]
+    ).reject { |nx, ny, nz| nx == x && ny == y && nz == z }
+      .map { |n| curr.fetch(n, INACTIVE) }
+      .select { |a| a == ACTIVE }
+      .size
 
-    active_cube = neigh_coords.map { |n| curr.fetch(n, INACTIVE) }.select { |a| a == ACTIVE }
-
-    if c == ACTIVE && active_cube.size.between?(2, 3)
-      nxt[[x, y, z]] = ACTIVE
-    elsif curr.fetch([x, y, z], INACTIVE) == INACTIVE && active_cube.size == 3
+    if (c == ACTIVE && active_cube_count.between?(2, 3)) ||
+       (curr.fetch([x, y, z], INACTIVE) == INACTIVE && active_cube_count == 3)
       nxt[[x, y, z]] = ACTIVE
     end
   end
 
   curr = nxt
-}
-
+end
 
 puts curr.values.select { |c| c == ACTIVE }.size
