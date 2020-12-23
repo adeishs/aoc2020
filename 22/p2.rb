3a421205c9ff35ee92e01700c08b06fce191d910
ade ishs <ade.ishs@strategicdata.com.au>
2020-12-23 20:24:25 +1100

    DRY

--- 22/p2.rb
+++ 22/p2.rb
@@ -6,26 +6,26 @@ def parse_player_inp(inp)
   lines.map { |line| line.chomp.to_i }
 end
 
+PLAYERS = [0, 1].freeze
+
 def subgame(decks)
   seen_deck = {}
-  while !decks[0].empty? && !decks[1].empty?
-    curr = [decks[0], decks[1]]
+  while PLAYERS.all? { |p| !decks[p].empty? }
+    curr = decks.map { |d| d }
     return [0, decks[0]] if seen_deck.has_key?(curr)
     seen_deck[curr] = true
 
-    card = {}
-    card[0] = decks[0].shift
-    card[1] = decks[1].shift
+    cards = []
+    PLAYERS.each { |p| cards << decks[p].shift }
 
-    if (0..1).all? { |p| decks[p].size >= card[p] }
-      winner = subgame([decks[0][0...card[0]].clone, decks[1][0...card[1]].clone])
-        .shift
+    if PLAYERS.all? { |p| decks[p].size >= cards[p] }
+      winner = subgame(PLAYERS.map { |p| decks[p][0...cards[p]] }).shift
     else
-      winner = card[0] > card[1] ? 0 : 1
+      winner = cards[0] > cards[1] ? 0 : 1
     end
     loser = 1 - winner
 
-    [winner, loser].each { |v| decks[winner] << card[v] }
+    [winner, loser].each { |v| decks[winner] << cards[v] }
     
     return [winner, decks[winner]] if decks[loser].empty?
   end
@@ -35,8 +35,7 @@ player_recs = $stdin.read.split("\n\n")
 decks = player_recs.map { |p| parse_player_inp(p).flatten }
 num_of_cards = decks.flatten.size
 
-(winner, decks) = subgame(decks)
+decks = subgame(decks).pop
 
-puts decks
-  .map.with_index { |v, k| v * (num_of_cards - k) }
+puts decks.map.with_index { |v, k| v * (num_of_cards - k) }
   .reduce { |m, o| m + o }
